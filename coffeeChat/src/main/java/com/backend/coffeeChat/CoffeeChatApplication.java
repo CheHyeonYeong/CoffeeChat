3658ce58f2b3fe1e186c0658928963be7b3cec18
Hyeon-Yeong Chae <62824602+CheHyeonYeong@users.noreply.github.com>
2025-07-01 10:57:14 +0900

    start : new project is started!!
    - 커피챗을 받는 BE 추가

---
+++ coffeeChat/src/main/java/com/backend/coffeeChat/CoffeeChatApplication.java
@@ -0,0 +1,13 @@
+package com.backend.coffeeChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeChatApplication.class, args);
+	}
+
+}
---
+++ coffeeChat/src/test/java/com/backend/coffeeChat/CoffeeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.coffeeChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
